34e1b35482fe64a62add690dfa4977487888133a
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-08 16:37:23 +0200

    feat: add base Entity abstract class

---
+++ GameLogic/Domain/Entity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.Domain
+{
+    public abstract class Entity
+    {
+        public float MaxHealth { get; private set;}
+        public float CurrentHealth { get; private set; }
+
+        public bool IsDead => CurrentHealth <= 0;
+
+        protected Entity(float maxHealth)
+        {
+            MaxHealth = maxHealth;
+            CurrentHealth = maxHealth;
+        }
+
+        public virtual void TakeDamage(float amount)
+        {
+            if (IsDead) { return; }
+            
+
+            CurrentHealth -= amount;
+            if (CurrentHealth < 0) { CurrentHealth = 0; }
+            if(CurrentHealth == 0) { Die(); }
+
+        }
+
+        protected abstract void Die();
+
+
+
+    }
+}
