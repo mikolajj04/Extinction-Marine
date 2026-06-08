a3acca52f91c506e3ee4733849859bf5d1813cf3
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-08 19:06:19 +0200

    feat(PlayerController): add basic event handling in PlayerController

--- Assets/_Project/Scripts/Gameplay/PlayerController.cs
+++ Assets/_Project/Scripts/Gameplay/PlayerController.cs
@@ -1,3 +1,4 @@
+using GameLogic.Core.Models;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,23 +11,34 @@ namespace ExtinctionMarine.Gameplay
         [Header("Movement Settings")]
         [SerializeField] private float moveSpeed = 5f;
 
+        private PlayerEntity logicData;
         private Rigidbody2D rb;
         private Vector2 moveInput;
+
+
+        public float CurrentHp => logicData.CurrentHealth;
+        public float MaxHp => logicData.MaxHealth;
+        public bool IsDead => logicData.IsDead;
 
         private void Awake()
         {
             
             rb = GetComponent<Rigidbody2D>();
+            logicData = new PlayerEntity();
         }
 
         public void OnMove(InputValue value)
         {
-         
+            if(IsDead) { return; }
             moveInput = value.Get<Vector2>();
         }
 
         private void FixedUpdate()
         {
+            if (IsDead)
+            {
+                rb.linearVelocity = Vector2.zero;
+            }
             MovePlayer();
         }
 
@@ -37,5 +49,24 @@ namespace ExtinctionMarine.Gameplay
 
             rb.linearVelocity = movement;
         }
+
+        public void ApplyDamage(float damageAmount)
+        {
+            if (logicData == null) { return; }
+            Debug.Log($"[Unity View] -HIT! HP before: {logicData.CurrentHealth}");
+            logicData.TakeDamage(damageAmount);
+            Debug.Log($"[Unity View] HP after: {logicData.CurrentHealth}");
+            if (logicData.IsDead)
+            {
+                HandleDeath();
+            }
+
+        }
+        private void HandleDeath()
+        {
+            Debug.LogWarning("[Unity View] TRIGGER GAME OVER: Marine has been killed");
+           
+        }
+
     }
 } 
