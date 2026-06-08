8240a7eaee297c9816f29cb9e224e0437abdc3ae
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-08 17:26:44 +0200

    feat(systems): add CombatEngine class

---
+++ GameLogic/Core/Systems/CombatEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameLogic.Core.Models;
+
+namespace GameLogic.Core.Systems
+{
+    public static class CombatEngine
+    {
+        public static void ResolveDamage<TAtacker, TTarger>(TAtacker atacker, TTarger target, float baseDamage)
+        where TAtacker : Entity
+        where TTarger : Entity
+        {
+
+
+            if (atacker.IsDead || target.IsDead) { return; }
+
+            float finalDamage = baseDamage;
+
+            target.TakeDamage(finalDamage);
+
+        }
+
+
+
+
+    }
+}
