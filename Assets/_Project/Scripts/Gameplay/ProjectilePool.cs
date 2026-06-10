323f0a761c2ed4aa3d7c68de6ac848995be62746
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-10 23:46:50 +0200

    feat(gameplay): introduce ProjectilePool for bullet management

---
+++ Assets/_Project/Scripts/Gameplay/ProjectilePool.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay
+{
+    public class ProjectilePool : MonoBehaviour
+    {
+        [Header("Pool Configuration")]
+        [SerializeField] private ProjectileController projectilePrefab;
+        [SerializeField] private int initialPoolSize = 50;
+
+        private Queue<ProjectileController> poolQueue = new Queue<ProjectileController>();
+
+        private void Start()
+        {
+            PreWarmPool();
+        }
+
+        
+        private void PreWarmPool()
+        {
+            for (int i = 0; i < initialPoolSize; i++)
+            {
+                ProjectileController bullet = Instantiate(projectilePrefab, transform);
+                bullet.gameObject.SetActive(false);
+                poolQueue.Enqueue(bullet);
+            }
+        }
+
+     
+        public void FireProjectile(Vector2 position, Vector2 direction)
+        {
+            ProjectileController bullet;
+
+         
+            if (poolQueue.Count > 0)
+            {
+                bullet = poolQueue.Dequeue();
+            }
+            else
+            {
+                Debug.LogWarning("[ProjectilePool] Pool is out! Dynamic allocation of a new projectile.");
+                bullet = Instantiate(projectilePrefab, transform);
+            }
+
+          
+            bullet.Initialize(position, direction, ReturnToPool);
+        }
+
+      
+        private void ReturnToPool(ProjectileController bullet)
+        {
+         
+            if (!poolQueue.Contains(bullet))
+            {
+                poolQueue.Enqueue(bullet);
+            }
+        }
+    }
+}
