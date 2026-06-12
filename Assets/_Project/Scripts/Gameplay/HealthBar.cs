26f7dfbaa2f8add8af5826de1020cafdb76b6947
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-12 21:26:48 +0200

    feat(gameplay): add HealthBar class

---
+++ Assets/_Project/Scripts/Gameplay/HealthBar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ExtinctionMarine.Gameplay.UI
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [SerializeField] private Image fillImage;
+
+        
+        public void UpdateBar(float currentHealth, float maxHealth)
+        {
+            
+            fillImage.fillAmount = currentHealth / maxHealth;
+
+            Debug.Log($"[HealthBar] UI updated. Current fill: {fillImage.fillAmount}");
+        }
+    }
+}
