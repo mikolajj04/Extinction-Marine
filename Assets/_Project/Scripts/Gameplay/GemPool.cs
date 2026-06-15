b5f9265cfdd86db75f09d9c29974a34e6709f76d
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-15 22:06:30 +0200

    feat(leveling):add ExpPool handling Exp Object-Pooling

---
+++ Assets/_Project/Scripts/Gameplay/GemPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay
+{
+    public class GemPool : MonoBehaviour
+    {
+        [SerializeField] private ExpGem gemPrefab;
+        [SerializeField] private int initialSize = 50;
+
+        private Queue<ExpGem> pool = new Queue<ExpGem>();
+
+      
+        private void OnEnable()
+        {
+            EnemyController.OnEnemyKilled += SpawnGemAtPosition;
+        }
+
+        private void OnDisable()
+        {
+            EnemyController.OnEnemyKilled -= SpawnGemAtPosition;
+        }
+
+        private void Start()
+        {
+            for (int i = 0; i < initialSize; i++)
+            {
+                CreateNewGem();
+            }
+        }
+
+        private ExpGem CreateNewGem()
+        {
+            ExpGem gem = Instantiate(gemPrefab, transform);
+            gem.gameObject.SetActive(false);
+            pool.Enqueue(gem);
+            return gem;
+        }
+
+        private void SpawnGemAtPosition(Vector3 deathPosition)
+        {
+            ExpGem gem = pool.Count > 0 ? pool.Dequeue() : CreateNewGem();
+            gem.Initialize(deathPosition, ReturnGem);
+        }
+
+        private void ReturnGem(ExpGem gem)
+        {
+            if (!pool.Contains(gem))
+            {
+                pool.Enqueue(gem);
+            }
+        }
+    }
+}
