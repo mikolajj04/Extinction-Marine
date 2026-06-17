dc2b76340ac1ce9e115e5b56164b4e1e877c089a
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-17 23:56:39 +0200

    feat(leveling): add base levelup screen

---
+++ Assets/_Project/Scripts/Gameplay/LevelUpScreen.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay.UI
+{
+    public class LevelUpScreen : MonoBehaviour
+    {
+        [Header("UI References")]
+        [SerializeField] private GameObject levelUpPanel;
+
+        private void Start()
+        {
+            
+            levelUpPanel.SetActive(false);
+        }
+
+        public void ShowLevelUpScreen()
+        {
+            levelUpPanel.SetActive(true);
+
+            
+            Time.timeScale = 0f;
+            Debug.Log("[LevelUpScreen] System upgrade initialized. Time frozen.");
+        }
+
+      
+        public void OnFireRateUpgradeChosen()
+        {
+            Debug.Log("[LevelUpScreen] Fire Rate upgrade selected.");
+            
+            ResumeGame();
+        }
+
+        public void OnSpeedUpgradeChosen()
+        {
+            Debug.Log("[LevelUpScreen] Speed upgrade selected.");
+            
+            ResumeGame();
+        }
+
+        private void ResumeGame()
+        {
+            levelUpPanel.SetActive(false);
+
+           
+            Time.timeScale = 1f;
+        }
+    }
+}
