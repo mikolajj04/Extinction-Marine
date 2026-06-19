fd1f5f328ed06577873cd79edd80639679e108cd
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-19 23:59:26 +0200

    refactor: move SpeedUpgrade from PlayerController.cs to distinct class

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/SpeedUpgrade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtinctionMarine.Gameplay;
+using ExtinctionMarine.Gameplay.Upgrades;
+
+namespace Assets._Project.Scripts.Gameplay.Upgrades
+{
+    public class SpeedUpgrade : IUpgrade
+    {
+        public string Title => "[ ENHANCE SERVOS ]";
+        public string Description => "Increases movement speed by 1.5.";
+
+        public void Apply(PlayerController player)
+        {
+            player.MoveSpeed += 1.5f;
+        }
+    }
+}
