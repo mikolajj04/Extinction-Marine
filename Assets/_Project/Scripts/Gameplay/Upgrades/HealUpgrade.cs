4aaa1e3a12a45dadfe3ed5cee2360be1ed5b5acc
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-20 00:43:57 +0200

    feat: add new HealUpgrade

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/HealUpgrade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtinctionMarine.Gameplay;
+using ExtinctionMarine.Gameplay.Upgrades;
+using UnityEngine;
+
+namespace Assets._Project.Scripts.Gameplay.Upgrades
+{
+    public class HealUpgrade : IUpgrade
+    {
+        public string Title => "[ I NEED MORE STIMS!! ]";
+        public string Description => "Instantly restores 80 HP";
+
+        public void Apply(PlayerController player)
+        {
+            player.LogicData.Heal(80f);
+            Debug.LogWarning("[PlayerController] Upgrade has been choosen!: Marine stimmed! +80HP!");
+            
+        }
+    }
+}
