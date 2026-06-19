b189b240d1263e11e89da13929df347922b9aae8
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-19 23:35:18 +0200

    feat: implement IUpgrade Interface for new marine upgrades

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/IUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtinctionMarine.Gameplay;
+
+namespace Assets._Project.Scripts.Gameplay.Upgrades
+{
+    public interface IUpgrade
+    {
+          string Title { get; }
+          string Description { get; }
+        void Apply(PlayerController player);
+    }
+}
