dc062d484b82cb412b2394fd63b4b4d4a43a7e8e
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-24 23:08:05 +0200

    chore(tester): commented CombatTester component

--- Assets/_Project/Scripts/Gameplay/CombatTester.cs
+++ Assets/_Project/Scripts/Gameplay/CombatTester.cs
@@ -1,48 +1,48 @@
-﻿using UnityEngine;
-using UnityEngine.InputSystem;
-using GameLogic.Core.Systems;
-using GameLogic.Core.Models;
+﻿//using UnityEngine;
+//using UnityEngine.InputSystem;
+//using GameLogic.Core.Systems;
+//using GameLogic.Core.Models;
 
-namespace ExtinctionMarine.Gameplay
-{
-    public class CombatTester : MonoBehaviour
-    {
-        [SerializeField] private PlayerController playerController;
-        private RaptorEntity temporaryRaptor;
+//namespace ExtinctionMarine.Gameplay
+//{
+//    public class CombatTester : MonoBehaviour
+//    {
+//        [SerializeField] private PlayerController playerController;
+//        private RaptorEntity temporaryRaptor;
 
-        private void Start()
-        {
-            temporaryRaptor = new RaptorEntity();
-            Debug.Log("[CombatTester] Test system started. Press space to inflict damage");
-        }
+//        private void Start()
+//        {
+//            temporaryRaptor = new RaptorEntity();
+//            Debug.Log("[CombatTester] Test system started. Press space to inflict damage");
+//        }
 
-        private void Update()
-        {
+//        private void Update()
+//        {
            
-            if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
-            {
-                ExecuteTestAttack();
-            }
-        }
+//            if (Keyboard.current != null && Keyboard.current.spaceKey.wasPressedThisFrame)
+//            {
+//                ExecuteTestAttack();
+//            }
+//        }
 
-        private void ExecuteTestAttack()
-        {
-            if (playerController == null)
-            {
-                Debug.LogError("[CombatTester] No refrence to PlayerController!");
-                return;
-            }
+//        private void ExecuteTestAttack()
+//        {
+//            if (playerController == null)
+//            {
+//                Debug.LogError("[CombatTester] No refrence to PlayerController!");
+//                return;
+//            }
 
-            if (playerController.IsDead)
-            {
-                Debug.LogWarning("[CombatTester] Player is dead. Test has ended.");
-                return;
-            }
+//            if (playerController.IsDead)
+//            {
+//                Debug.LogWarning("[CombatTester] Player is dead. Test has ended.");
+//                return;
+//            }
 
-            float raptorDamage = 25f;
-            Debug.LogWarning($"\n[CombatTester] !!! Raptor inficts {raptorDamage} damage points!!!");
+//            float raptorDamage = 25f;
+//            Debug.LogWarning($"\n[CombatTester] !!! Raptor inficts {raptorDamage} damage points!!!");
 
-            playerController.ApplyDamage(raptorDamage);
-        }
-    }
-}
+//            playerController.ApplyDamage(raptorDamage);
+//        }
+//    }
+//}
