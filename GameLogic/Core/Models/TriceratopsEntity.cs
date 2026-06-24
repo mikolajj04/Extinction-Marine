82b87bd2fa28126d9dacd981af9b255545ad03dc
mikolajj04 <Mikolajjussak@gmail.com>
2026-06-24 23:59:38 +0200

    refactor(models): adjust base enemies to new constructor

---
+++ GameLogic/Core/Models/TriceratopsEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.Core.Models
+{
+    internal class TriceratopsEntity : DinosaurEntity
+    {
+        public TriceratopsEntity() : base(40f, 25f, 3.5f, 40f)
+        {
+
+        }
+
+        protected override void Die()
+        {
+            
+        }
+    }
+}
