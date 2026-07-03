e5faf7e505e508d52ee5ef037c1e35a086c55a58
mikolajj04 <Mikolajjussak@gmail.com>
2026-07-03 22:52:39 +0200

    feat(upgrades): add Piercing Rounds (PierceUpgrade) to the upgrade manager

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/PierceUpgrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtinctionMarine.Gameplay;
+using ExtinctionMarine.Gameplay.Upgrades;
+using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay.Upgrades
+{
+    internal class PierceUpgrade : IUpgrade
+    {
+        public string Title => "[ PIERCING BULLETS! ]";
+        public string Description => "+1 penetration for bullets";
+        public int CurrentLevel { get; set; } = 0;
+        public int MaxLevel => 4;
+
+        public void Apply(PlayerController player)
+        {
+            player.ApplyPenetrationUpgrade();
+
+        }
+    }
+}
+
