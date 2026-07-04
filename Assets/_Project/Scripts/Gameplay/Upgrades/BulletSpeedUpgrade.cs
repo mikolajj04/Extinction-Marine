8105f5753c2342591e711f56a24551a18e5bbe00
mikolajj04 <Mikolajjussak@gmail.com>
2026-07-04 22:34:33 +0200

    feat: add BulletSpeedUpgrade class

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/BulletSpeedUpgrade.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay.Upgrades
+{
+    public class BulletSpeedUpgrade : IUpgrade
+    {
+        public string Title => "[ SUPERSONIC BULLETS ]";
+        public string Description => "Increases speed of bullets by X.";
+        public int CurrentLevel { get; set; } = 1;
+        public int MaxLevel => 100;
+        public void Apply(PlayerController player)
+        {
+
+            player.ApplyBulletSpeedUpgrade(0.2f);
+
+
+        }
+    }
+}
