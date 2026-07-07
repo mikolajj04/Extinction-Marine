0b204383ca5b9e2ddab4b191d48a18b8a550d9b2
mikolajj04 <Mikolajjussak@gmail.com>
2026-07-07 23:40:11 +0200

    feat(enemies): add new Stegosaurus enemy

---
+++ GameLogic/Core/Models/StegosaurusEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameLogic.Core.Models
+{
+    public class StegosaurusEntity : DinosaurEntity
+    {
+        public StegosaurusEntity() : base(100f, 35f, 3f, 65f)
+        {
+
+        }
+
+        protected override void Die()
+        {
+
+        }
+    }
+}
