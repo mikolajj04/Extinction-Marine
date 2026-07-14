376eb27d15eef89486ca3ae2a4574f0202a916ee
mikolajj04 <Mikolajjussak@gmail.com>
2026-07-14 23:54:12 +0200

    feat(upgrades): add ConcussiveShellsUpgrade card

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/ConcussiveShellsUpgrade.cs
@@ -0,0 +1,20 @@
+﻿using ExtinctionMarine.Gameplay.Controllers;
+
+namespace ExtinctionMarine.Gameplay.Upgrades
+{
+    public class ConcussiveShells : IUpgrade
+    {
+        public string Title => "[ Concussive Shells ]";
+        public string Description => "Adds +2.5 kinetic impact to bullets, knocking enemies back.";
+
+        public int CurrentLevel { get; set; } = 1;
+        public int MaxLevel => 8;
+
+        public void Apply(PlayerController player)
+        {
+
+            player.ApplyConcussiveShells();
+
+        }
+    }
+}
