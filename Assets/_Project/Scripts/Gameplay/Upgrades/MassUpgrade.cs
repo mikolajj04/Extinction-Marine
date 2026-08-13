392474329770a9d6b701ae765e15d6d13c46f78b
mikolajj04 <Mikolajjussak@gmail.com>
2026-08-13 23:55:00 +0200

    feat: add MassUpgrade card

---
+++ Assets/_Project/Scripts/Gameplay/Upgrades/MassUpgrade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExtinctionMarine.Gameplay.Controllers;
+using ExtinctionMarine.Gameplay.Upgrades;
+
+namespace ExtinctionMarine.Gameplay.Upgrades
+{
+        public class MassUpgrade : IUpgrade
+        {
+            public string Title => "[ TITANIUM EXOSKELETON ]";
+            public string Description => "Increases Marine's mass by 15. High knockback resistance and heavy pushing power.";
+
+            public int CurrentLevel { get; set; } = 0;
+            public int MaxLevel => 5;
+
+            public void Apply(PlayerController player)
+            {
+                
+                player.ApplyMassUpgrade(15f);
+            }
+        }
+}
+
