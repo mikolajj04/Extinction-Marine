09c032bcc9add327a99b3e4ef68a35422273057c
mikolajj04 <Mikolajjussak@gmail.com>
2026-08-22 21:39:53 +0200

    feat: update CarnotaurusEntity class

--- GameLogic/Core/Models/CarnotaurusEntity.cs
+++ GameLogic/Core/Models/CarnotaurusEntity.cs
@@ -6,11 +6,46 @@ namespace GameLogic.Core.Models
 {
     public class CarnotaurusEntity : DinosaurEntity
     {
-        public CarnotaurusEntity() : base(1000f, 70f, 7.5f, 500f)
+        private bool isCharging = false;
+        private float ChargeDuration = 2f;
+        private float ChargeCooldown = 10f;
+        private float timer;
+        private float baseSpeed;
+        public CarnotaurusEntity() : base(1000f, 70f, 6f, 500f)
         {
             IsImmuneToKnockback = true;
+            IsImpenetrable = true;
+            baseSpeed = Speed;
+            timer = ChargeCooldown;
+            isCharging = false;
+        }
+
+        public override void Tick(float deltaTime)
+        {
+            timer -= deltaTime;
+            if (timer <= 0)
+            {
+                if (isCharging)
+                {
+                    isCharging = false;
+                    Speed = baseSpeed;
+                    timer = ChargeCooldown;
+                }
+                else
+                {
+                    isCharging = true;
+                    Speed = baseSpeed * 2f;
+                    timer = ChargeDuration;
+                }
+            }
+        }
+
+
+        public void Tick()
+        {
 
         }
+        
 
         protected override void Die() { }
     }
