e98fd28c416f19ef460ab69bb2167f3a77f80f9d
mikolajj04 <Mikolajjussak@gmail.com>
2026-08-30 23:14:49 +0200

    feat: improve MainMenuController with mainPanel.SetActive() method

--- Assets/_Project/Scripts/Gameplay/Controllers/MainMenuController.cs
+++ Assets/_Project/Scripts/Gameplay/Controllers/MainMenuController.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEditor.Search;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace ExtinctionMarine.Gameplay.UI
@@ -9,11 +12,38 @@ namespace ExtinctionMarine.Gameplay.UI
         [Tooltip("Enter the name of scene (ex. CoreGame)")]
         [SerializeField] private string gameSceneName = "SampleScene";
 
+        [Header("UI Panels")]
+        [Tooltip("Connect main menu here (Play, Armory, Settings, Quit, etc...)")]
+        [SerializeField] private GameObject mainPanel;
+
+        [Tooltip("Connect Armory panel here")]
+        [SerializeField] private GameObject armoryPanel;
+
+
+        [Tooltip("Connect Setting panel here ")]
+        [SerializeField] private GameObject settingsPanel;
+
         private void Start()
         {
             Time.timeScale = 1f;
+            ShowPanel(mainPanel);
         }
 
+        public void OpenArmory()
+        {
+            Debug.Log("[MainMenu] Accessing Armory Database...");
+            ShowPanel(armoryPanel);
+        }
+
+        public void OpenSetting()
+        {
+            Debug.Log("[MainMenu] Accessing Setting...");
+            ShowPanel(settingsPanel);
+        }
+        public void BackToMainMenu()
+        {
+            ShowPanel(mainPanel);
+        }
         public void StartGame()
         {
             Debug.Log("[MainMenu] Deploying Marine to the hazard zone...");
@@ -28,6 +58,13 @@ namespace ExtinctionMarine.Gameplay.UI
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
 #endif
+        }
+        private void ShowPanel(GameObject panelToShow)
+        {
+            if (mainPanel != null) mainPanel.SetActive(mainPanel == panelToShow);
+            if (armoryPanel != null) armoryPanel.SetActive(armoryPanel == panelToShow);
+            if (settingsPanel != null) settingsPanel.SetActive(settingsPanel == panelToShow);
+
         }
     }
 }
