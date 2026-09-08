0e7f4576c5d3709e90779fc831db6ab407df143e
mikolajj04 <Mikolajjussak@gmail.com>
2026-09-08 17:54:49 +0200

    feat(menu): add AudioManagerclass and OnClick/OnHover audio clips
    -Connect audio clips with proper events

---
+++ Assets/_Project/Scripts/Gameplay/Systems/AudioManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace ExtinctionMarine.Gameplay.Systems
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager Instance { get; private set; }
+        [Header("Audio Sources")]
+        [SerializeField] private AudioSource uiSource;
+
+        [Header("UI Clips")]
+        [SerializeField] private AudioClip hoverClip;
+        [SerializeField] private AudioClip clickClip;
+        private void Awake()
+        {
+            if(Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void PlayUIHover() => uiSource.PlayOneShot(hoverClip);
+        public void PlayUIClick() => uiSource.PlayOneShot(clickClip);
+    }
+}
